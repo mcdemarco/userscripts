a8efd2dfdbc05de5bec50047fe2bc3e77c51d026
M. C. DeMarco <mcd@mcdemarco.net>
2021-02-25 07:09:12 -0500

    Updates from 0.8.9

--- pinteresting/pinterest-remove-unwanted-pins.js
+++ pinteresting/pinterest-remove-unwanted-pins.js
@@ -2,11 +2,12 @@
 // @name        Pinterest - Remove Unwanted Pins
 // @namespace   valacar.pinterest.remove-picked-for-you
 // @author      mcd
-// @version     0.9.2
-// @description Remove unwanted pins on Pinterest, like Promoted pins, Product pins, and Ideas for you.
+// @version     0.9.9
+// @description Remove unwanted pins on Pinterest, like Promoted pins, Product pins, and Ideas for you. (edited from valacar's 0.8.9)
 // @include     https://*.pinterest.tld/*
-// @exclude     /^https://(help|blog|about|buisness|developers|engineering|careers|policy|offsite)\.pinterest/
+// @exclude     /^https://(help|blog|about|buisness|developers|engineering|careers|policy|offsite|newsroom)\.pinterest/
 // @grant       none
+// @run-at      document-start
 // @noframes
 // @license     MIT
 // @compatible  firefox Firefox
@@ -16,24 +17,35 @@
 (function() {
   "use strict";
 
-  const DEBUGGING = 0;
-
-  // allow debugging keys without logging messages
+  const DEBUGGING = 0; // output debugging info to the developer console
   const ENABLE_KEYS = 0; // r = reveal (unhide), s = search (find and hide)
+  const SHOW_BAD_PINS = 0; // show bad pins, outlined and faded
 
   const badStoryType = [
     "real_time_boards", // ideas for you
+    "BUBBLE_ONE_COL", // ideas for you
     "BUBBLE_TRAY_CAROUSEL", // ideas you might love
     "PINART_INTEREST", // topics for you
     "single_column_recommended_board",
-    "recommended_searches" // searches to try
+    "recommended_searches", // searches to try
+    "RECOMMENDED_TOPICS",
+    "explore_board_ideas" // new ideas for your board
+  ];
+
+  const badRecommendation = [
+    "INTENTIONAL_DISTRIBUTION_TOPICS_STORY_PINS", // story
+    "INTENTIONAL_DISTRIBUTION_TOPICS_VIDEO", // some videos ads
+    "GRAPHSAGE_PRODUCT_PINS", // product pins
+    //"GEMINI_INTERESTS_FRESH", // often ad-like, but unsure about enabling
+    //"FRESH_TEXT_EMBEDDINGS",
   ];
 
   const badStoryTypeSet = new Set(badStoryType);
+  const badRecommendationSet = new Set(badRecommendation);
 
   const hidePinClass = "unwanted";
 
-  let cachedHandler;
+  let cachedReactInfo;
   let cachedGrid;
 
   /* ---------------------------- */
@@ -49,7 +61,10 @@
     }
   }
 
-  appendStyle(`.${hidePinClass} { visibility: hidden !important; }`);
+  appendStyle(`
+.${hidePinClass} { display: none !important; }
+.dbg { outline: 2px dashed red; opacity: 0.2; }
+`);
 
   const debugLog = DEBUGGING ? console.log.bind(console, "[unwanted]") : function() {};
 
@@ -59,9 +74,11 @@
       data.promoter ||
       pin.querySelector('[data-test-id="one-tap-desktop"]') ||
       pin.querySelector('[data-test-id="one-tap-desktop-carousel"]') ||
+      pin.querySelector('[title="Test GPT"]') || 
+      pin.querySelector('[data-test-id="otpp"]') || 
       pin.querySelector('[data-test-id="oneTapPromotedPin"]') || 
       pin.querySelector('[data-test-id="pinrep-video"]');
-    if (result) debugLog("--- removed PROMOTED pin:", data.domain);
+    if (result) debugLog("--- removed PROMOTED pin:", data.domain || "unknown domain");
     return result;
   }
 
@@ -76,67 +93,143 @@
     return result;
   }
 
-  function isCommercePin(data)
+  function hasPriceTagIcon(pin)
   {
-    let result = (data.shopping_flags && data.shopping_flags.length > 0)
-      || data.buyable_product;
+		return false;
+    const paths = pin.querySelectorAll('path');
+    for (let path of paths) {
+      if (path && path.hasAttribute('d')) {
+        // check the first few curves and lines of the svg icon (hopefully unique)
+        if (path.attributes.d.textContent.startsWith(
+          'M6 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m7.36-6.2l8.84 8.84'
+        ))
+        {
+          debugLog("::: price tag icon found");
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function isCommercePin(data, pin)
+  {
+		return false;
+    let result = ((data.shopping_flags && data.shopping_flags.length > 0)
+      || data.buyable_product) || hasPriceTagIcon(pin);
     if (result) {
       debugLog("--- removed COMMERCE pin:",
-        data.domain,
+        data.domain || "unknown domain",
         data.buyable_product ? "(buyable)" : "");
     }
     return result;
   }
 
-  function getEventHandler(pin)
+  function isBadRecommendation(data)
+  {
+    let result = data.recommendation_reason &&
+      badRecommendationSet.has(data.recommendation_reason.reason);
+    if (result) {
+      debugLog("--- removed RECOMMENDED pin:", data.recommendation_reason.reason);
+    }
+    return result;
+  }
+
+  function getReactInfo(pin)
   {
     return Object.keys(pin).find(
-      prop => prop.startsWith("__reactEventHandlers")
+      prop => prop.startsWith("__reactProps")
     );
+  }
+
+  function getChildrenPropsData(target)
+  {
+    let path;
+    if (target && "children" in target && target.children) {
+      if (Array.isArray(target.children)) {
+        for (let child of target.children) {
+          path = getPathToPinDataFromChild(child);
+          if (path) {
+            if ("id" in path) {
+              return path;
+            }
+          }
+        }
+      } else {
+        path = getPathToPinDataFromChild(target.children);
+        if (path) {
+          if ("id" in path) {
+            return path;
+          }
+        }
+      }
+    }
+  }
+
+  function getPathToPinDataFromChild(obj)
+  {
+    if (obj && "props" in obj) {
+      if ("data" in obj.props) {
+        return obj.props.data;
+      }
+      if ("pin" in obj.props) {
+        return obj.props.pin;
+      }
+    }
   }
 
   function getPinData(pin)
   {
-    let handler = cachedHandler || getEventHandler(pin);
-    if (!handler) {
-      debugLog("!!! can't find React event handler");
+    let reactInfo = cachedReactInfo || getReactInfo(pin);
+    if (!reactInfo) {
       return false;
     }
-    cachedHandler = handler;
-    //debugLog("::: found react event handler", handler);
+    cachedReactInfo = reactInfo;
     let data;
     let target;
     try {
-      if (pin.hasAttribute("data-grid-item") || pin.classList.contains("Collection-Item")) {
-        target = pin;
-        data = target[handler].children.props.data;
+      // try pinWrapper
+      target = pin.querySelector('[data-test-id="pinWrapper"]');
+      if (target) {
+        data = getChildrenPropsData(target[reactInfo]);
+        if (data) {
+          return data;
+        }
+      }
+      // try "root" of pin
+      target = pin;
+      if (!"children" in target[reactInfo]) {
+        return false;
+      }
+      data = target[reactInfo].children.props.data;
+      if (data && "id" in data) {
+        return data;
+      }
+      // try pin.firstchild
+      target = pin.firstChild;
+      if (target && target[reactInfo].children && target[reactInfo].children.props) {
+        data = target[reactInfo].children.props.data;
         if (data && "id" in data) {
           return data;
         }
-        target = pin.firstChild;
-        if (target) {
-          data = target[handler].children.props.data;
+      }
+      // try pin.firstchild.firstChild
+      target = pin.firstChild.firstChild;
+      if (target && target[reactInfo].children && target[reactInfo].children.props) {
+        data = target[reactInfo].children.props.data;
+        if (data && "id" in data) {
+          return data;
+        }
+        if (Array.isArray(target[reactInfo].children) && target[reactInfo].children[0].props) {
+          data = target[reactInfo].children[0].props.data;
           if (data && "id" in data) {
             return data;
           }
         }
-        target = pin.firstChild.firstChild;
-        if (target) {
-          data = target[handler].children.props.data;
-          if (data && "id" in data) {
-            return data;
-          }
-          data = target[handler].children[0].props.data;
-          if (data && "id" in data) {
-            return data;
-          }
-        }
-      } else {
-        debugLog("!!! couldn't find [data-grid-item] or .Collection-Item");
       }
     }
     catch(err) {
-      debugLog("!!! Caught error:", err);
+      debugLog("!!! Caught error:", err, pin);
     }
   }
 
@@ -144,7 +237,12 @@
   {
     let data = getPinData(pin);
     if (!data) return false;
-    if (isPromotedPin(data, pin) || isIdeaPin(data) ) {
+    if (
+      isCommercePin(data, pin) ||
+      isPromotedPin(data, pin) ||
+      isIdeaPin(data) ||
+      isBadRecommendation(data)
+    ) {
       return true;
     }
   }
@@ -152,7 +250,11 @@
   function modifyBadPin(pin)
   {
     if (isBadPin(pin)) {
-      pin.classList.add(hidePinClass);
+      if (SHOW_BAD_PINS) {
+        pin.classList.add("dbg");
+      } else {
+        pin.classList.add(hidePinClass);
+      }
     }
   }
 
@@ -185,6 +287,7 @@
 
   function searchAndDestroy()
   {
+		/*
 		var i, elements = document.querySelectorAll('[data-root-margin]');
 		//remove sticky bars.  don't need to check for sticky anymore but do.
 		for (i = 0; i < elements.length; i++) {
@@ -194,10 +297,15 @@
 		for (i = 0; i < elements.length; i++) {
 			elements[i].parentNode.remove();
 		}
+		elements = document.querySelectorAll('body a[role="tab"]');
+		for (i = 0; i < elements.length; i++) {
+			elements[i].parentNode.remove();
+		}
 		elements = document.querySelectorAll('div.fixedHeader');
 		for (i = 0; i < elements.length; i++) {
 			elements[i].remove();
 		}
+		*/
 
     debugLog(">>> searching and destroying");
     const grid = findGrid();
@@ -219,26 +327,6 @@
           }
         }
       }
-    }
-  }
-
-  // Simplify creating a Mutation Observer (from document root)
-  function createObserver(target, callbackFunction, rules)
-  {
-    let targetElement;
-    if (typeof target === "string") {
-      targetElement = document.querySelector(target);
-    } else {
-      // XXX: assume it's an element for now
-      targetElement = target;
-      debugLog(targetElement);
-    }
-    if (targetElement) {
-      const observer = new MutationObserver(callbackFunction);
-      observer.observe(targetElement, rules);
-      debugLog("::: Observer created for", target);
-    } else {
-      debugLog("::: Couldn't create observer for", target);
     }
   }
 
@@ -274,10 +362,15 @@
   }
 
   window.addEventListener("load", () => {
-    createObserver(".mainContainer", mutationCallback, {
-      childList: true,
-      subtree: true
-    });
+    const target = ".mainContainer";
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+      const observer = new MutationObserver(mutationCallback);
+      observer.observe(targetElement, { childList: true, subtree: true });
+      debugLog("::: Observer created for", target);
+    } else {
+      debugLog("::: Couldn't create observer for", target);
+    }
     searchAndDestroy();
   });
 
